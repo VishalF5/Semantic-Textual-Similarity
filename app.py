217b6f14b7a39641f7914ffb40b274333a950a11
VishalF5 <89625260+VishalF5@users.noreply.github.com>
2022-11-01 17:26:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,31 @@
 import streamlit as st
-from sklearn.feature_extraction.text import CountVectorizer
+import tensorflow
+import tensorflow_hub as hub
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 
+encoder_url = 'https://tfhub.dev/google/universal-sentence-encoder/4' 
 
-cv = CountVectorizer()
-
-
-def similarity(sen1 ,sen2):
-    score = cosine_similarity(sen1 ,sen2)
-    return score
+encoder = hub.load(encoder_url)
 
 
-input_text1 = st.text_area("Enter")
+
+# Calculating Cosine Similarity between two sentences
+def get_similarity(sentence_a, sentence_b):
+    embed_a = encoder([sentence_a])
+    embed_b = encoder([sentence_b])
+    similarity = cosine_similarity(embed_a, embed_b)[0][0]
+    return f'The similarity score between sentence A and sentence B is: "{similarity:.2f}"'
+
+st.title("Text Similarity")
+
+input_text1 = st.text_area("Enter ")
 input_text2 = st.text_area("Enter your text here")
 
-input_text1 = [input_text1]
-input_text2 = [input_text2]
 
 
 if st.button('Predict'):
-    input_text1 = cv.fit_transform(input_text1).toarray()
-    input_text2 = cv.fit_transform(input_text2).toarray()
-    res = similarity(input_text1 ,input_text2)
+    res = get_similarity(input_text1 ,input_text2)
+
     st.write(res)
 
