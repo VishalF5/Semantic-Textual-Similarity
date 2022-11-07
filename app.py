f8689ff61a999bf91180d4a919faa902a61d67d6
VishalF5 <89625260+VishalF5@users.noreply.github.com>
2022-11-07 14:17:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import tensorflow_hub as hub
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-st.cache()
 # Encoder
 encoder_url = 'https://tfhub.dev/google/universal-sentence-encoder/4' 
 
@@ -21,12 +20,12 @@ def get_similarity(sentence_a, sentence_b):
 # Interface
 st.title("Text Similarity")
 
-input_text1 = st.text_input("Enter First Sentence : ")
-input_text2 = st.text_input("Enter Second Sentence : ")
+input_text1 = st.text_input("Enter First Sentence : " ,label_visibility = 'visible')
+input_text2 = st.text_input("Enter Second Sentence : " ,label_visibility = 'visible')
 
+res = get_similarity(input_text1 ,input_text2)
 
 
 if st.button('Predict'):
-    res = get_similarity(input_text1 ,input_text2)
 
     st.success(res)
