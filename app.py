b3e7b6b29bcacfb9fd89714239fc3998f2c18784
VishalF5 <89625260+VishalF5@users.noreply.github.com>
2022-11-07 14:02:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import tensorflow_hub as hub
 from sklearn.metrics.pairwise import cosine_similarity
 
+
+st.cache()
+# Encoder
 encoder_url = 'https://tfhub.dev/google/universal-sentence-encoder/4' 
 
 encoder = hub.load(encoder_url)
@@ -13,17 +16,17 @@ def get_similarity(sentence_a, sentence_b):
     embed_a = encoder([sentence_a])
     embed_b = encoder([sentence_b])
     similarity = cosine_similarity(embed_a, embed_b)[0][0]
-    return f'The similarity score between sentence A and sentence B is: "{similarity:.2f}"'
+    return f'The similarity score is : "{similarity:.2f}"'
 
+# Interface
 st.title("Text Similarity")
 
-input_text1 = st.text_area("Enter ")
-input_text2 = st.text_area("Enter your text here")
+input_text1 = st.text_input("Enter First Sentence : ")
+input_text2 = st.text_input("Enter Second Sentence : ")
 
 
 
 if st.button('Predict'):
     res = get_similarity(input_text1 ,input_text2)
 
-    st.write(res)
-
+    st.success(res)
